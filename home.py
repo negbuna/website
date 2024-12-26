b5d680d8afc1848f393af90afc37eb3ac4d33813
nathan <negbuna@users.noreply.github.com>
2024-12-25 19:42:32 -0500

    flask -> gunicorn

--- home.py
+++ home.py
@@ -7,5 +7,25 @@ def home():
    return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from gunicorn.app.base import BaseApplication
 
+    class FlaskApplication(BaseApplication):
+        # app.run(True)
+        def __init__(self, app, options=None):
+            self.application = app
+            self.options = options or {}
+            super().__init__()
+
+        def load(self):
+            return self.application
+
+        def load_config(self):
+            for key, value in self.options.items():
+                self.cfg.set(key, value)
+
+    options = {
+        'bind': '0.0.0.0:5000',
+        'workers': 2,
+    }
+    GunicornApp = FlaskApplication(app, options)
+    GunicornApp.run()
